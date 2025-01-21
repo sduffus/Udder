1ffadf91f2b347f0923e81be4260eebb7be09c97
Shaun Duffus <sduffus007@gmail.com>
2025-01-21 00:46:52 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Shaun Duffus on 2025-01-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
